c9d9f7ee12e8e74751d5c15fedbc836f034a60d3
Albert Lu <socerball30@gmail.com>
2023-03-30 14:04:21 -0400

    updated testing

---
+++ src/Attribute_graphs/testing_path.py
@@ -0,0 +1,72 @@
+"""
+Ideal test pattern for attribute graphs:
+
+Is the similarity between "good" stories better than the similarity between bad?
+"""
+
+"""
+base = base_story_path
+good = good_story_path
+bad = bad_story_path
+
+(Consider a global variable for these paths)
+
+Construct graphs (v, e) 
+(Test this individually)
+
+(Save ideal graphs for comparison)
+
+base_graph = construct_graph(base)
+good_graph = construct_graph(good)
+bad_graph = construct_graph(bad)
+
+Test similarities:
+
+Compare_graph(base_graph, good_graph)
+Compare_graph(base_graph, bad_graph)
+
+Functions:
+
+Compare attributes and relationships
+relationships only
+attributes only
+
+"""
+
+
+"""
+Ideal test pattern for event graphs:
+
+Is the similarity between "good" stories better than the similarity between bad?
+"""
+
+"""
+base = base_story_path
+good = good_story_path
+bad = bad_story_path
+
+(Consider a global variable for these paths)
+
+Construct lists (v, e) 
+(Test this individually)
+
+(Save ideal lists for further analysis and comparison)
+
+base_graph = construct_graph(base)
+good_graph = construct_graph(good)
+bad_graph = construct_graph(bad)
+
+Test similarities:
+
+(Both semantic and exact match)
+
+Compare_graph(base_graph, good_graph)
+Compare_graph(base_graph, bad_graph)
+
+Functions:
+Create pipeline with (s, v, o)
+Create pipeline with (s, v, o, m)
+
+Create a main file that does all of this
+"""
+
